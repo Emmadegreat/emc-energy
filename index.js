0c7a3a3986bd32f2bb93366423c686aefbce152d
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2025-03-25 18:51:53 +0000

    add

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 const openMobileMenu =()=> {
-    holder = document.getElementById("holder");
-    holder.classList.add("active");
+  holder = document.getElementById("holder");
+  holder.classList.add("active");
 }
 
 const closeMobileMenu=()=> {
-    holder = document.getElementById("holder");
-    holder.classList.remove("active");
+  holder = document.getElementById("holder");
+  holder.classList.remove("active");
 }
 
 document.getElementById("date").innerHTML = new Date().getFullYear();
